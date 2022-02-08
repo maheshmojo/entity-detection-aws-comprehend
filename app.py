fcf04050301c0d23dc7552d678a2b4fd1d5dd9ab
Mahesh waran <maheshmac21@gmail.com>
2022-02-09 00:23:28 +0530

    app update

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ def predict():
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message] 
-		data = obj.get()['Body'].read().decode("utf-8")
-    	text_redact = comprehend.detect_sentiment(Text=data , LanguageCode='en')
-		my_prediction = json.dumps(text_redact)
+		data = obj.get()['Body'].read().decode("utf-8")    	
+		my_prediction = j comprehend.detect_sentiment(Text=data , LanguageCode='en')
 	return render_template('result.html',prediction = my_prediction)
 
 if __name__ == '__main__':
