59ab0ab3bb1020de826f242e2054aa9a590716f8
Mahesh waran <maheshmac21@gmail.com>
2022-02-08 23:56:09 +0530

    update app file

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # Importing essential libraries
+from email import message
 from flask import Flask, render_template, request
 import pickle
 import boto3
 
 comprehend = boto3.client(service_name='comprehend', region_name='ap-south-1',aws_access_key_id = "AKIAZBPWXPNP3X2DRRBJ", aws_secret_access_key = "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")
 
-# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
-filename = 'spam-sms-mnb-model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
-cv = pickle.load(open('cv-transform.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,10 +15,11 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-    	message = request.form[comprehend.detect_sentiment(Text=text, LanguageCode='en')]
-    	data = [message]
-    	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
+    	message = request.form['message']
+		data = [message] 
+		data = obj.get()['Body'].read().decode("utf-8")
+    	text_redact = comprehend.detect_sentiment(Text=data , LanguageCode='en')
+		my_prediction = json.dumps(text_redact)
     return render_template('result.html', prediction=my_prediction)
    
 
